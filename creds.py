951394f8df05c6f6a2f88907153ee9089d6a3b9d
bot-dd <botdd445@gmail.com>
2025-05-16 23:03:46 +0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,12 +5,12 @@ load_dotenv(find_dotenv())
 
 
 class cred:
-    BOT_NAME = os.getenv("BOT_NAME")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # From botfather
+    BOT_NAME = os.getenv("BOT_NAME", "subtr")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7560563197:AAHmmIygYMElUtUPnYc2Iu05v5y1KOTWpnY")  # From botfather
     API_ID = os.getenv(
-        "API_ID"
+        "26649585"
     )  # "Get this value from my.telegram.org! Please do not steal"
     API_HASH = os.getenv(
-        "API_HASH"
+        "588a3ea6fd01ae88bd2e10fed7d55b2c"
     )  # "Get this value from my.telegram.org! Please do not steal"
-    DB_URL = os.getenv("DB_URL")  # From Firebase database
+    DB_URL = os.getenv("mongodb+srv://rohanahamed75:gt4RXJZ1mUtOh4Xv@mmtg.0ong5.mongodb.net/?retryWrites=true&w=majority&appName=mmtg")  # From Firebase database
